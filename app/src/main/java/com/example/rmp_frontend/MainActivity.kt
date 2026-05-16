99f95d22ae10dc0a07ba67cc22fac446327a40b0
mrcdssclss <alexandrevnas@gmail.com>
2026-05-16 14:39:17 +0300

    feat: added login screen

--- app/src/main/java/com/example/rmp_frontend/MainActivity.kt
+++ app/src/main/java/com/example/rmp_frontend/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.rmp_frontend.ui.screens.login.LoginScreen
 import com.example.rmp_frontend.ui.theme.RMPfrontendTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RMPfrontendTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                LoginScreen()
             }
         }
     }
